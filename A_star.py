c9042dd006ba0b981abfb32316c2a2d6618001a8
odacolquhoun <101328280+odacolquhoun@users.noreply.github.com>
2022-10-19 10:49:41 +0200

    Add files via upload

---
+++ A_star.py
@@ -0,0 +1,227 @@
+import numpy as np
+from Map import *
+np.set_printoptions(threshold=np.inf, linewidth=300)
+from PIL import Image
+
+# We got the code from https://github.com/BaijayantaRoy/Medium-Article/blob/master/A_Star.ipynb and made some adjustments
+
+class Node:
+    """
+        A node class for A* Pathfinding
+        parent is parent of the current Node
+        position is current position of the Node in the maze
+        g is cost from start to current Node
+        h is heuristic based estimated cost for current Node to end Node
+        f is total cost of present node i.e. :  f = g + h
+    """
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+    def __eq__(self, other):
+        return self.position == other.position
+
+# This function returns the path of the search
+def return_path(current_node,maze):
+    path = []
+    no_rows, no_columns = np.shape(maze)
+
+    # Instead of initialized result maze with -1, we use the original maze
+    result = map_int_np
+    current = current_node
+    while current is not None:
+        path.append(current.position)
+        current = current.parent
+    # Return reversed path as we need to show from start to end path
+    path = path[::-1]
+    
+    # We update the path of start to end found by A-star search with every step getting the value of 5 (because of coloring later)
+    for i in range(len(path)):
+        result[path[i][0]][path[i][1]] = 5
+
+    return result
+
+
+def search(maze, start, end):
+    """
+        Returns a list of tuples as a path from the given start to the given end in the given maze
+        :param maze:
+        :param cost
+        :param start:
+        :param end:
+        :return:
+    """
+    
+    # Create start and end node with initized values for g, h and f
+    start_node = Node(None, tuple(start))
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, tuple(end))
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both yet_to_visit and visited list
+    # In this list we will put all nodes that are yet_to_visit for exploration
+    # From here we will find the lowest cost node to expand next
+    yet_to_visit_list = []  
+
+    # in this list we will put all nodes that are already explored, so that we don't explore it again
+    visited_list = [] 
+    
+    # Add the start node
+    yet_to_visit_list.append(start_node)
+    
+    # Adding a stop condition. This is to avoid any infinite loop and stop execution after some reasonable number of steps
+    outer_iterations = 0
+    max_iterations = (len(maze) // 2) ** 10
+
+    # There are 4 possible movements from each position: up, left, down, right
+    move  =  [[-1, 0 ], # go up
+              [ 0, -1], # go left
+              [ 1, 0 ], # go down
+              [ 0, 1 ]] # go right
+
+
+    """
+        1) We first get the current node by comparing all f cost and selecting the lowest cost node for further expansion
+        2) Check max iteration reached or not . Set a message and stop execution
+        3) Remove the selected node from yet_to_visit list and add this node to visited list
+        4) Perform Goal test and return the path, else perform below steps
+        5) For selected node find out all children (use move to find children)
+            a) get the current postion for the selected node (this becomes parent node for the children)
+            b) check if a valid position exist (boundary will make few nodes invalid)
+            c) if any node is a wall then ignore that
+            d) add to valid children node list for the selected parent
+            
+            For all the children node
+                a) if child in visited list then ignore it and try next node
+                b) calculate child node g, h and f values
+                c) if child in yet_to_visit list then ignore it
+                d) else move the child to yet_to_visit list
+    """
+    # Find number of rows and columns 
+    no_rows, no_columns = np.shape(maze)
+    
+    # Loop until you find the end
+    
+    while len(yet_to_visit_list) > 0:
+        
+        # Every time any node is referred from yet_to_visit list, counter of limit operation incremented
+        outer_iterations += 1    
+        
+        # Get the current node
+        current_node = yet_to_visit_list[0]
+        current_index = 0
+        for index, item in enumerate(yet_to_visit_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+                
+        # If we hit this point, the computation cost is too high or there may be no solution
+        if outer_iterations > max_iterations:
+            print ("giving up on pathfinding too many iterations")
+            return return_path(current_node,maze)
+
+        # Pop current node out off yet_to_visit list, add to visited list
+        yet_to_visit_list.pop(current_index)
+        visited_list.append(current_node)
+
+        # Test if goal is reached or not, if yes then return the path
+        if current_node == end_node:
+            return return_path(current_node,maze)
+
+        # Generate children from all adjacent squares
+        children = []
+
+        for new_position in move: 
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure it is within range (within maze boundary)
+            if (node_position[0] > (no_rows - 1) or 
+                node_position[0] < 0 or 
+                node_position[1] > (no_columns -1) or 
+                node_position[1] < 0):
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] == -1:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            
+            # Child is on the visited list (search entire visited list)
+            if len([visited_child for visited_child in visited_list if visited_child == child]) > 0:
+                continue
+
+            # Create the f, g, and h values (g = current nodes cost + cell cost of the child)
+            child.g = current_node.g + maze[child.position[0]][child.position[1]] 
+            
+            # Heuristic costs calculated here, this is using euclidean distance
+            # Here we fixed a flaw in the source code by adding a square-root around the expression
+            child.h = np.sqrt(((child.position[0] - end_node.position[0]) ** 2) + 
+                       ((child.position[1] - end_node.position[1]) ** 2)) 
+
+            child.f = child.g + child.h
+
+            # Child is already in the yet_to_visit list and g cost is already lower
+            if len([i for i in yet_to_visit_list if child == i and child.g > i.g]) > 0:
+                continue
+
+            # Add the child to the yet_to_visit list
+            yet_to_visit_list.append(child)
+
+def show_path(map):
+     # Define width and height of image
+        width = map.shape[1]
+        height = map.shape[0]
+        # Define scale of the image
+        scale = 20
+        # Create an all-yellow image
+        image = Image.new('RGB', (width * scale, height * scale), (255, 255, 0))
+        # Load image
+        pixels = image.load()
+
+        # Define what colors to give to different values of the string map (undefined values will remain yellow, this is
+        # how the yellow path is painted)
+        colors = {-1: (255, 0, 0), 1: (215, 215, 215), 2: (166, 166, 166), 3: (96, 96, 96), 4: (36, 36, 36), 
+                    5: (255, 251, 0) }
+                 
+        # Go through image and set pixel color for every position
+        for y in range(height):
+            for x in range(width):
+                if map[y][x] not in colors: continue
+                for i in range(scale):
+                    for j in range(scale):
+                        pixels[x * scale + i, y * scale + j] = colors[map[y][x]]
+        # Show image
+        image.show()
+
+
+if __name__ == '__main__':
+
+    # Initializing a map-object and retrieves the map with ints
+    # Choose task 1, 2, 3 or 4
+    map_obj = Map_Obj(task=1) 
+    map_int_np, map_str_np = map_obj.get_maps()
+
+    # Initializating parameters for the search functon
+    maze = map_int_np
+    start = map_obj.get_start_pos()
+    end = map_obj.get_goal_pos()
+    path = search(maze, start, end)
+    show_path(path)
+    
+
+    
+    
